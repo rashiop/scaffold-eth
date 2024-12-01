d7fcb948c62be2a455810770ac6ef88aef5f2f0c
Poppy <red.alpacca@gmail.com>
2024-12-01 20:25:45 +0400

    feat(1155): enable trade

--- packages/hardhat/contracts/Forge.sol
+++ packages/hardhat/contracts/Forge.sol
@@ -5,11 +5,14 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "./Items.sol";
 
-contract Forge {
+contract Forge is ERC1155Holder {
   event Forged(address indexed user, uint256 indexed itemId);
+  event Traded(address indexed user, uint256 indexed itemId, uint256 indexed materialId);
   
   error IncorrectMaterial(address user, uint256[] items, uint256[] values);
   error InsufficientMaterial(address user, uint256[] items, uint256[] values);
+  error InvalidTrade(address user, uint256 itemId, uint256 materialId);
+  error PausedMaterial();
 
   Items private _item;
 
@@ -49,6 +52,19 @@ contract Forge {
 
     _item.burnBatch(msg.sender, items, values);
     _item.mintItem(msg.sender, _item.MYSTERIOUS_CHARM());
+  }
 
+  function tradeItemToMaterial(uint256 itemId, uint256 materialId) external {
+    require(itemId >= 3 && itemId <= 6 && materialId < 3 && materialId >= 0, InvalidTrade(msg.sender, itemId, materialId));
+    require(!_item.paused(), PausedMaterial());
+
+    if (_item.balanceOf(address(this), materialId) < 1) {
+      _item.safeTransferFrom(msg.sender, address(this), itemId, 1, "");
+      _item.mintItem(msg.sender, materialId);
+    } else {
+      _item.safeTransferFrom(address(this), msg.sender, materialId, 1, "");
+      _item.safeTransferFrom(msg.sender, address(this), itemId, 1, "");
+    }
+    emit Traded(msg.sender, itemId, materialId);
   }
 }
